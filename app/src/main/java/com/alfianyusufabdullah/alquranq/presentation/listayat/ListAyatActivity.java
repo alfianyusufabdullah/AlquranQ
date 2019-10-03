18eb64b2090a4469dc046d6c279ba7f9ab5fb9e0
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2019-10-03 17:23:02 +0700

    migrate to AndroidX

--- app/src/main/java/com/alfianyusufabdullah/alquranq/presentation/listayat/ListAyatActivity.java
+++ app/src/main/java/com/alfianyusufabdullah/alquranq/presentation/listayat/ListAyatActivity.java
@@ -1,9 +1,9 @@
 package com.alfianyusufabdullah.alquranq.presentation.listayat;
 
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.alfianyusufabdullah.alquranq.R;
