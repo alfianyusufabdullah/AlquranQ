a9cad2b8abf2fc6385d2769142c1168f08421b15
Achmad Fachrudin <a2fachrudin@gmail.com>
2019-10-07 15:26:19 +0700

    delete unused import

--- app/src/main/java/com/alfianyusufabdullah/alquranq/App.java
+++ app/src/main/java/com/alfianyusufabdullah/alquranq/App.java
@@ -3,17 +3,12 @@ package com.alfianyusufabdullah.alquranq;
 import android.app.Application;
 import android.content.res.Resources;
 
-import com.alfianyusufabdullah.alquranq.model.ModelAyat;
-import com.alfianyusufabdullah.alquranq.model.ModelSurah;
 import com.alfianyusufabdullah.alquranq.database.DatabaseHelper;
 import com.alfianyusufabdullah.alquranq.utils.PreferenceApp;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by jonesrandom on 2/20/18.
